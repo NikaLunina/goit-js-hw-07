d37f05c07e631a1e5c78127e28e69633678267e5
LuninaNika <nikakulini4@gmail.com>
2023-01-21 20:49:59 +0100

    hw7

---
+++ js/02-lightbox.js
@@ -0,0 +1,27 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const imgList = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", imgList);
+
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  gallery.on("show.Simplelightbox");
+});
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
